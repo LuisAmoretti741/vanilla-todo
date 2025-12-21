1a03b2475ea536eec318e3f95f1bbeb9fb38e8b7
LuisYsrael741 <amorettiysrael3@gmail.com>
2025-12-21 22:02:17 +0100

    commit della settimana scorsa

--- detail-script.js
+++ detail-script.js
@@ -2,26 +2,45 @@ function displayTodo(todo) {
     const titleHeader = document.getElementById('todo-title');
     titleHeader.innerHTML = todo.title;
 
-    const descriptionMain = document.getElementById('todo-description');
-    descriptionMain.innerHTML = 'Description: ' + todo.description;
+    const descriptionSpan = document.getElementById('todo-description');
+    descriptionSpan.innerHTML = todo.description;
 
-    const createDataMain = document.getElementById('todo-createDate');
-    createDataMain.innerHTML = 'Creation Date: ' + todo.creationDate;
+    const creationDateSpan = document.getElementById('todo-creation-date');
+    creationDateSpan.innerHTML = formaDate(todo.creationDate);
 
-    const endDataMain = document.getElementById('todo-endDate');
-    endDataMain.innerHTML = 'End Date: ' + todo.endDate;
+    const endDateSpan = document.getElementById('todo-end-date');
+    endDateSpan.innerHTML = formaDate(todo.endDate);
 
-    const colorMain = document.getElementById('todo-color');
-    colorMain.innerHTML = 'Color: ' + todo.color;
+    const colorDiv = document.getElementById('todo-color');
+    colorDiv.style.backgroundColor = todo.color;
 
-    const doneMain = document.getElementById('todo-done');
-    doneMain.innerHTML = 'Done: ' + todo.done;
+    const doneSpan = document.getElementById('todo-done');
+    if (todo.done) {
+        doneSpan.innerHTML = 'completato';
+    } else {
+        doneSpan.innerHTML = 'da completare'
+    }
+
+}
+
+function formaDate(dateISO) {
+    const date = new Date(dateISO);
+
+    // const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+
+    // return formattedDate;
+
+    const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+    };
+
+    return date.toLocaleDateString("it-IT", options);
 }
 
 const searchParams = new URLSearchParams(window.location.search);
 
 const id = searchParams.get('todoId');
-
-console.log(id);
 
 getTodo(id).then(result => displayTodo(result));
--- script.js
+++ script.js
@@ -1,29 +1,85 @@
-function displayTodos(todos) {
+function displayTodos(todos){
+
     const todosContainer = document.getElementById('todos-container');
     todosContainer.innerHTML = "";
 
     for (const todo of todos) {
+        
         const card = document.createElement('div');
         card.classList.add('todo-card');
 
+        const colorAndTitleDiv = document.createElement('div');
+        colorAndTitleDiv.classList.add('color-and-title');
+
+        const colorDot = document.createElement('div');
+        colorDot.classList.add('colored-dot');
+        colorDot.style.backgroundColor = todo.color;
+
+        colorAndTitleDiv.appendChild(colorDot);
+
         const titleSpan = document.createElement('span');
-        titleSpan.appendChild(document.createTextNode('Title: ' + todo.title));
+        titleSpan.appendChild(document.createTextNode(todo.title));
+        titleSpan.style.textDecoration = todo.done ? 'line-through' : 'none';
 
-        card.appendChild(titleSpan);
+        colorAndTitleDiv.appendChild(titleSpan);
 
-        const detailBtn = document.createElement('button');
-        detailBtn.appendChild(document.createTextNode('⮞  '));
-        detailBtn.classList.add('detail-btn');
-        detailBtn.addEventListener('click', () => {
-            window.location.assign('./detail.html?todoId=' + todo.id);
-        })
+        card.appendChild(colorAndTitleDiv);
 
-        card.appendChild(detailBtn);
+        // const detailBtn = document.createElement('button');
+        // detailBtn.appendChild(document.createTextNode("🠊"));
+        // detailBtn.classList.add("detail-btn");
+        // detailBtn.addEventListener('click', () => {
+        //     window.location.assign('./detail.html?todoId=' + todo.id)
+        // })
+
+        // card.appendChild(detailBtn);
+
+        const detailLink = document.createElement('a');
+        detailLink.appendChild(document.createTextNode("🠊"));
+        detailLink.classList.add("detail-link");
+        detailLink.href = './detail.html?todoId=' + todo.id;
+
+        card.appendChild(detailLink);
 
         todosContainer.appendChild(card);
+
     }
-    
+
+}
+
+let todos = []
+
+getAllTodos().then(results => {
+    todos = results;
+    displayTodos(todos)
+})
+
+
+function orderByTitle() {
+    todos.sort((t1, t2) => t1.title.localeCompare(t2.title));
+    displayTodos(todos);
+}
+
+function compareDates(t1, t2) {
+    const date1 = t1.creationDate;
+    const date2 = t2.creationDate;
+    const dateObj1 = new Date(date1);
+    const dateObj2 = new Date(date2);
+    const time1 = dateObj1.getTime();
+    const time2 = dateObj2.getTime();
+    return time2 - time1; 
+}
+
+function orderByCreationDate(){
+    todos.sort(compareDates);
+    displayTodos(todos);
 }
 
 
-getAllTodos().then(results => displayTodos(results))
+// due tasti nella home (lista di todo)
+// 1) mette in ordine alfabetico
+// 2) mette in ordine dal più nuovo al più vecchio in base alla creationDate
+
+// 0) finire il task dato a lezione (quanti giorni mancano alla scadenza??)
+// 1) personalizzare graficamente l'app
+// 2) aggiungere un bottone delete nella pagina di dettaglio
